2d1fdc33a3756d7cfb14ab4b098aaacc3f80f4b7
RahulGattu912 <rahulgattu912@gmail.com>
2025-11-09 11:29:39 +0530

    added vpn security group and rule to connect to alb

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -1,51 +1,65 @@
 # create 3 security groups: frontend, backend, mysql
 
 module "mysql_sg" {
-    source          = "git::https://github.com/RahulGattu912/terraform-modules.git//terraform-aws-security-group?ref=main"
+    source          = var.sg_source
     environment     = var.environment
     project_name    = var.project_name
     sg_name         = "mysql"
-    sg_description  = "Created for MySQL instances int expense dev"
+    sg_description  = "Created for MySQL instances in expense dev"
     vpc_id          = data.aws_ssm_parameter.vpc_id.value
     common_tags     = var.common_tags
 }
 
 module "backend_sg" {
-    source          = "git::https://github.com/RahulGattu912/terraform-modules.git//terraform-aws-security-group?ref=main"
+    source          = var.sg_source
     environment     = var.environment
     project_name    = var.project_name
     sg_name         = "backend"
-    sg_description  = "Created for backend instances int expense dev"
+    sg_description  = "Created for backend instances in expense dev"
     vpc_id          = data.aws_ssm_parameter.vpc_id.value
     common_tags     = var.common_tags
 }
 
 module "frontend_sg" {
-    source          = "git::https://github.com/RahulGattu912/terraform-modules.git//terraform-aws-security-group?ref=main"
+    source          = var.sg_source
     environment     = var.environment
     project_name    = var.project_name
     sg_name         = "frontend"
-    sg_description  = "Created for frontend instances int expense dev"
+    sg_description  = "Created for frontend instances in expense dev"
     vpc_id          = data.aws_ssm_parameter.vpc_id.value
     common_tags     = var.common_tags
 }
 
 module "bastion_sg" {
-    source          = "git::https://github.com/RahulGattu912/terraform-modules.git//terraform-aws-security-group?ref=main"
+    source          = var.sg_source
     environment     = var.environment
     project_name    = var.project_name
     sg_name         = "bastion"
-    sg_description  = "Created for bastion instances int expense dev"
+    sg_description  = "Created for bastion instances in expense dev"
     vpc_id          = data.aws_ssm_parameter.vpc_id.value
     common_tags     = var.common_tags
 }
 
 module "app_alb_sg" {
-    source          = "git::https://github.com/RahulGattu912/terraform-modules.git//terraform-aws-security-group?ref=main"
+    source          = var.sg_source
     environment     = var.environment
     project_name    = var.project_name
     sg_name         = "app-alb"
-    sg_description  = "Created for backend ALB instances int expense dev"
+    sg_description  = "Created for backend ALB instances in expense dev"
+    vpc_id          = data.aws_ssm_parameter.vpc_id.value
+    common_tags     = var.common_tags
+}
+
+# vpn security group. ports -> 22,443,1194,943
+# 22 - to login to server
+# 443 - to login to browser
+# 1194, 943 - internal ports/administrative ports
+module "vpn_sg" {
+    source          = var.sg_source
+    environment     = var.environment
+    project_name    = var.project_name
+    sg_name         = "vpn"
+    sg_description  = "Created for vpn instances in expense dev"
     vpc_id          = data.aws_ssm_parameter.vpc_id.value
     common_tags     = var.common_tags
 }
@@ -70,3 +84,53 @@ resource "aws_security_group_rule" "bastion_public" {
   security_group_id =  module.backend_sg_id.sg_id
 }
 
+resource "aws_security_group_rule" "vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # usually it should be static IP
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  security_group_id =  module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  # usually it should be static IP
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  security_group_id =  module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_943" {
+  type              = "ingress"
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  # usually it should be static IP
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  security_group_id =  module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "tcp"
+  # usually it should be static IP
+  cidr_blocks       = [ "0.0.0.0/0" ]
+  security_group_id =  module.vpn_sg.sg_id
+}
+
+# alb accepting traffic from vpn
+resource "aws_security_group_rule" "app_alb_vpn" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  # usually it should be static IP
+  source_security_group_id = module.vpn_sg.sg_id # app alb should accept traffic from vpn
+  security_group_id =  module.vpn_sg.sg_id
+}
--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -13,3 +13,7 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "sg_source" {
+    default = "git::https://github.com/RahulGattu912/terraform-modules.git//terraform-aws-security-group?ref=main"
+}
