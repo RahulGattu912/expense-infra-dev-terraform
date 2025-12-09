c23ac14713b9e7045525033db250b0a6232e4344
RahulGattu912 <rahulgattu912@gmail.com>
2025-12-09 21:18:15 +0530

    changed backend

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -7,4 +7,4 @@ dnf install ansible -y
 # pull
 ansible-pull -i localhost, -U https://github.com/RahulGattu912/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
 # this pulls main.yaml from the repo and applies it on that server 
-# -e component=backend is the command line variable
+# -e component=backend is the command line variable 
